c98b6553aa9c5240324e12e4b48115526e2a2ec7
Md. Ali Azgor <aliazgoryeasin@gmail.com>
2023-04-06 01:37:03 +0600

    the routing and preview order page making done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
-import Shop from './components/Shop/Shop'
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
 return (
     <div className="App">
       <Header></Header>
-      <Shop></Shop>
+      <Outlet></Outlet>
     </div>
   )
 }
